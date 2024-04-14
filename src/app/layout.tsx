351c784514d65f6c740c7635d8c8182fbb40acab
Amin <amin.arabi@gmail.com>
2024-04-13 21:08:36 -0400

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+    title: "Multicultural Business Solutions",
+    description:
+        "We help our clients understand why culturally different people interact professionally in ways that seem strange and wrong to them.",
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return <main>
+      Multicutural Business Solutions
+    </main>
+}
